798b41c468024d0bb033236acf6a30211ba11039
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-05-09 15:47:02 -0300

    req1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,84 @@
-describe('1. Teste o componente `<App.js />`', () => {
-  test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('1. Teste o componente <App.js />', () => {
+  test('se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavPok = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavPok).toBeInTheDocument();
+  });
+
+  test('se a aplicação é direcionada para a página inicial ao clicar no Nav Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // ir para outra pag, para de lá testar o 'link home' corretamente
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+
+    // confirmar se cheguei no 'about'
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    // ver se linkHome está disponível
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    // clicar no linkHome
+    userEvent.click(linkHome);
+
+    // confirmar se cheguei na 'home'
+    const path = history.location.pathname;
+    expect(path).toBe('/');
+  });
+
+  test('se a aplicação é direcionada para a página about ao clicar no Nav About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // ver se link About está disponível
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    // clicar no link About
+    userEvent.click(linkAbout);
+
+    // confirmar se cheguei na 'about'
+    const path = history.location.pathname;
+    expect(path).toBe('/about');
+  });
+
+  test('se a aplicação é direcionada para a página PokeFav ao clicar no Nav', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // ver se link PokFav está disponível
+    const linkPokFav = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkPokFav).toBeInTheDocument();
+
+    // clicar no linkPokFav
+    userEvent.click(linkPokFav);
+
+    // confirmar se cheguei na 'PokFav'
+    const path = history.location.pathname;
+    expect(path).toBe('/favorites');
+  });
+
+  test('se a aplicação é direcionada para a página NotFound ao entrar em uma URL', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/url-desconhecida');
+
+    const notFoundImg = screen.getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    expect(notFoundImg).toBeInTheDocument();
+  });
 });
